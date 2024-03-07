09c779375a4ea1d068b408daf06b605e121a647b
lysialeao <lysialeao@gmail.com>
2024-03-07 00:32:19 -0300

    feat: :sparkles: creates the list of students

--- src/views/Students/View.vue
+++ src/views/Students/View.vue
@@ -1,6 +1,70 @@
 
 <template>
-    <main>
-      <h1>this is students page</h1>
-    </main>
-  </template>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h4>Students</h4>
+                <RouterLink to="/student/create" class="btn btn-primary float-end">
+                    Add student
+                </RouterLink>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th> ID </th>
+                            <th>First Name </th>
+                            <th>Last Name </th>
+                        </tr>
+                    </thead>
+                    <tbody v-if="this.students.length > 0">
+                        <tr v-for="(student, index) in this.students" :key="index">
+                            <td>{{ student.id }}</td>
+                            <td>{{ student.first_name }}</td>
+                            <td>{{ student.last_name }}</td>
+                            <td>{{ student.email }}</td>
+                            <td>
+                                <RouterLink to="/" class="btn btn-success float-end">
+                                    Edit
+                                </RouterLink>
+                                <button class="btn btn-danger float-end">
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tbody v-else>
+                        <tr>
+                            <td colspan="6">
+                                Loading...
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+    export default {
+        name: 'students',
+        data(){
+            return {
+                students: []
+            }
+        },
+        mounted(){
+            this.getStudents()
+        },
+        methods: {
+            getStudents(){
+                axios.get('https://reqres.in/api/users')
+                    .then( res => {
+                        this.students = res.data.data
+                        console.log(res)
+                    })
+            }
+        }
+    }
+</script>
